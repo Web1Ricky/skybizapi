2901d732b01fa1b40e89a5a68e617c5f6025a500
RickyRakhmatP <ricky.rakhmatpr@gmail.com>
2023-05-04 12:43:41 +0700

    feature onhand 3

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit651bdb8ef1d56aab765798c8b8276c36
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit651bdb8ef1d56aab765798c8b8276c36', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit651bdb8ef1d56aab765798c8b8276c36', 'loadClassLoader'));
